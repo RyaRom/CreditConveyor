aec37f4b4364e8791075c02fe1d39bf8fa404efd
Roman Ryabikin <riabikin.roman@gmail.com>
2024-08-14 20:30:27 +0300

    add /deal/application endpoint

---
+++ deal/src/main/java/com/deal/config/ConveyorClient.java
@@ -0,0 +1,19 @@
+package com.deal.config;
+
+import com.deal.model.dto.CreditDTO;
+import com.deal.model.dto.LoanApplicationRequestDTO;
+import com.deal.model.dto.LoanOfferDTO;
+import com.deal.model.dto.ScoringDataDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@FeignClient(name = "conveyor-client", configuration = FeignConfig.class)
+public interface ConveyorClient {
+    @PostMapping("/conveyor/offers")
+    List<LoanOfferDTO> createOffers(LoanApplicationRequestDTO loanApplicationRequestDTO);
+
+    @PostMapping("/conveyor/calculation")
+    CreditDTO calculateCredit(ScoringDataDTO scoringData);
+}
--- deal/src/main/java/com/deal/controller/DealController.java
+++ deal/src/main/java/com/deal/controller/DealController.java
@@ -2,6 +2,8 @@ package com.deal.controller;
 
 import com.deal.model.dto.LoanApplicationRequestDTO;
 import com.deal.model.dto.LoanOfferDTO;
+import com.deal.service.DealService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class DealController {
+    private final DealService dealService;
+
     @PostMapping("/deal/application")
     public ResponseEntity<List<LoanOfferDTO>> generateOffers(@RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
-
+        return ResponseEntity.ok(dealService.generateOffers(loanApplicationRequestDTO));
     }
 }
