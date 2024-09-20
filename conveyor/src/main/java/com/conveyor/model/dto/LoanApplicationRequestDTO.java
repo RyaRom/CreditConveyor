89ad59e2d3d4644a5f8dd7f953a5cc6354ee9ab8
Roman Ryabikin <riabikin.roman@gmail.com>
2024-09-20 20:30:33 +0300

    refactor: replace @Data to record and Double to BigDecimal
    replace Double to BigDecimal in conveyor module

--- conveyor/src/main/java/com/conveyor/model/dto/LoanApplicationRequestDTO.java
+++ conveyor/src/main/java/com/conveyor/model/dto/LoanApplicationRequestDTO.java
@@ -6,56 +6,54 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
-import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Data
 @Builder
 @Schema(title = "Loan Application Request", description = "Short application info.")
-public class LoanApplicationRequestDTO {
+public record LoanApplicationRequestDTO(
+        @Schema(description = "Requested loan amount.", example = "1000000", minimum = "10000")
+        @NotNull
+        @Min(10000)
+        BigDecimal amount,
 
-    @Schema(description = "Requested loan amount.", example = "1000000", minimum = "10000")
-    @NotNull
-    @Min(10000)
-    private Double amount;
+        @Schema(description = "Requested loan term (months).", example = "24", minimum = "6")
+        @NotNull
+        @Min(6)
+        Integer term,
 
-    @Schema(description = "Requested loan term (months).", example = "24", minimum = "6")
-    @NotNull
-    @Min(6)
-    private Integer term;
+        @Schema(description = "Client's first name", example = "Ivan", pattern = "[A-Za-z\\-]{2,30}")
+        @NotNull
+        @Pattern(regexp = "[A-Za-z\\-]{2,30}")
+        String firstName,
 
-    @Schema(description = "Client's first name", example = "Ivan", pattern = "[A-Za-z\\-]{2,30}")
-    @NotNull
-    @Pattern(regexp = "[A-Za-z\\-]{2,30}")
-    private String firstName;
+        @Schema(description = "Client's last name", example = "Ivanov", pattern = "[A-Za-z\\-]{2,30}")
+        @NotNull
+        @Pattern(regexp = "[A-Za-z\\-]{2,30}")
+        String lastName,
 
-    @Schema(description = "Client's last name", example = "Ivanov", pattern = "[A-Za-z\\-]{2,30}")
-    @NotNull
-    @Pattern(regexp = "[A-Za-z\\-]{2,30}")
-    private String lastName;
+        @Schema(description = "Client's middle name (if present).", example = "Ivanonovich", pattern = "[A-Za-z]{2,30}")
+        @Pattern(regexp = "[A-Za-z]{2,30}")
+        String middleName,
 
-    @Schema(description = "Client's middle name (if present).", example = "Ivanonovich", pattern = "[A-Za-z]{2,30}")
-    @Pattern(regexp = "[A-Za-z]{2,30}")
-    private String middleName;
+        @Schema(description = "Client's email.", example = "iivanov@email.ru")
+        @NotNull
+        @Email
+        String email,
 
-    @Schema(description = "Client's email.", example = "iivanov@email.ru")
-    @NotNull
-    @Email
-    private String email;
+        @Schema(description = "Client's birthdate", example = "1996-11-27", format = "date")
+        @NotNull
+        LocalDate birthdate,
 
-    @Schema(description = "Client's birthdate", example = "1996-11-27", format = "date")
-    @NotNull
-    private LocalDate birthdate;
+        @Schema(description = "Client's passport series", example = "1234", pattern = "[0-9]{4}")
+        @NotNull
+        @Pattern(regexp = "[0-9]{4}")
+        String passportSeries,
 
-    @Schema(description = "Client's passport series", example = "1234", pattern = "[0-9]{4}")
-    @NotNull
-    @Pattern(regexp = "[0-9]{4}")
-    private String passportSeries;
-
-    @Schema(description = "Client's passport number", example = "123456", pattern = "[0-9]{6}")
-    @NotNull
-    @Pattern(regexp = "[0-9]{6}")
-    private String passportNumber;
-
+        @Schema(description = "Client's passport number", example = "123456", pattern = "[0-9]{6}")
+        @NotNull
+        @Pattern(regexp = "[0-9]{6}")
+        String passportNumber
+) {
 }
--- conveyor/src/main/java/com/conveyor/model/dto/PaymentScheduleElementDTO.java
+++ conveyor/src/main/java/com/conveyor/model/dto/PaymentScheduleElementDTO.java
@@ -2,30 +2,29 @@ package com.conveyor.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
-import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Data
 @Builder
 @Schema(title = "Payment Schedule Element", description = "Payment schedule element.")
-public class PaymentScheduleElementDTO {
+public record PaymentScheduleElementDTO(
+        @Schema(description = "Payment sequence number.", example = "1")
+        Integer number,
 
-    @Schema(description = "Payment sequence number.", example = "1")
-    private Integer number;
+        @Schema(description = "Payment date.", example = "2022-06-01", format = "date")
+        LocalDate date,
 
-    @Schema(description = "Payment date.", example = "2022-06-01", format = "date")
-    private LocalDate date;
+        @Schema(description = "Total payment amount.", example = "12000.00")
+        BigDecimal totalPayment,
 
-    @Schema(description = "Total payment amount.", example = "12000.00")
-    private Double totalPayment;
+        @Schema(description = "Interest payment amount.", example = "12000.00")
+        BigDecimal interestPayment,
 
-    @Schema(description = "Interest payment amount.", example = "12000.00")
-    private Double interestPayment;
+        @Schema(description = "Main debt payment amount.", example = "8000.00")
+        BigDecimal debtPayment,
 
-    @Schema(description = "Main debt payment amount.", example = "8000.00")
-    private Double debtPayment;
-
-    @Schema(description = "Remaining debt amount.", example = "92000.00")
-    private Double remainingDebt;
+        @Schema(description = "Remaining debt amount.", example = "92000.00")
+        BigDecimal remainingDebt
+) {
 }
